1316011d7a2916278ed7d3329394c9276c1b9c95
Voytecky <wojciechbobel@gmail.com>
2021-10-25 13:37:22 +0200

    ML4

--- src/app/models/musicmodel.model.ts
+++ src/app/models/musicmodel.model.ts
@@ -2,10 +2,12 @@ export class MusicModel{
     public album:string;
     public artist:string;
     public imgCover:string;
+    public x?:string;
 
-    constructor(album:string, artist:string, imgCover:string,){
+    constructor(album:string, artist:string, imgCover:string, x?:string){
         this.album = album;
         this.artist = artist;
         this.imgCover = imgCover;
+        this.x = x;
     }
 }
